648c09e9d847a4fdc516fb5ec56353b9fce19b34
kobank <wagaocarvalho@gmail.comm>
2023-09-13 14:41:16 -0300

    create: migrations

---
+++ db/migration/000001_initial_tables.up.sql
@@ -0,0 +1,8 @@
+\c email_sender
+
+CREATE TABLE "emails" (
+    idEmail SERIAL PRIMARY KEY NOT NULL,
+    dateSendEmail TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    subjectEmail VARCHAR(100),
+    messageEmail VARCHAR(250) NOT NULL
+);
